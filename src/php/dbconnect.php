691c8c7d54fc495929e79370fc8cfb606d1b94ef
Andrew7733 <milleran@g.cofc.edu>
2018-05-07 22:54:39 -0400

    Delete dbconnect.php

--- src/php/dbconnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mysqli = new mysqli('11.66.0.24', 'andrew23_CSCI332');
-
-if ($mysqli->connect_errno) {
-    echo "Errno: " . $mysqli->connect_errno . "</br>";
-    echo "Error: " . $mysqli->connect_error . "</br>";
-    exit;
-}
-?>
